65b3ca88c15e449d055ba29e7609e7dfe3949941
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-09 14:05:01 -0300

    api connect

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined'
+     && weather.main.temp >= 30 
+     ? 'warm' : '' ">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..." 
+          v-model="query"
+          @keypress="fetchWeather"
+          />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Belém, BR</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="location"> {{weather.name}}, {{weather.sys.country}} </div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temp"> {{Math.round(weather.main.temp)}}°c </div>
+          <div class="weather"> {{weather.weather[0].main}} </div>
         </div>
       </div>
     </main>
@@ -26,7 +34,35 @@ export default {
   name: 'App',
   data () {
     return{
-      api_key:'5ecb6ed7997eae26a6c9e2111974ecc1'
+      api_key:'5ecb6ed7997eae26a6c9e2111974ecc1',
+      url_base:"https://api.openweathermap.org/data/2.5/",
+      query:'',
+      weather:{}
+    }
+  },
+  methods:{
+    fetchWeather(e){
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res =>{
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results
+    },
+    dateBuilder(){
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
@@ -46,6 +82,9 @@ export default {
     background-size: cover;
     background-position: bottom;
     transition: 0.4s;
+  }
+  #app.warm{
+    background-image: url('./assets/warm-bg.jpg');
   }
 
   main{
