48c6e655a4836c682876459ce8086f327d661e23
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-09 12:08:00 -0300

    up

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return{
+      api_key:'5ecb6ed7997eae26a6c9e2111974ecc1'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
